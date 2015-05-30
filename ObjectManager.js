13560a0e1dffab56055ccfe7d297c6110613e2c6
Ivan Makhotin <i.makhotin@yandex.ru>
2015-05-30 15:54:12 +0500

    first commit

---
+++ ObjectManager.js
@@ -0,0 +1,87 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64],
+            zoom: 2
+        })
+    document.getElementById('plains_edges').onclick = function () {
+        objectManager = new ymaps.ObjectManager({});
+        myMap.geoObjects.removeAll();
+        myMap.geoObjects.add(objectManager);
+
+        $.ajax({
+        url: "plains_edges.json"
+        }).done(function(data) {
+            objectManager.add(data);
+        });
+    };
+
+    document.getElementById('sea_edges').onclick = function () {
+        objectManager = new ymaps.ObjectManager({});
+        myMap.geoObjects.removeAll();
+        myMap.geoObjects.add(objectManager);
+
+        $.ajax({
+        url: "sea_edges.json"
+        }).done(function(data) {
+            objectManager.add(data);
+        });
+    };
+
+    document.getElementById('buses_circles').onclick = function () {
+        objectManager = new ymaps.ObjectManager({});
+        myMap.geoObjects.removeAll();
+        myMap.geoObjects.add(objectManager);
+
+        $.ajax({
+        url: "buses_circles.json"
+        }).done(function(data) {
+            objectManager.add(data);
+        });
+    };
+    document.getElementById('plains_circles').onclick = function () {
+        objectManager = new ymaps.ObjectManager({});
+        myMap.geoObjects.removeAll();
+        myMap.geoObjects.add(objectManager);
+
+        $.ajax({
+        url: "plains_circles.json"
+        }).done(function(data) {
+            objectManager.add(data);
+        });
+    };
+    document.getElementById('trains_circles').onclick = function () {
+        objectManager = new ymaps.ObjectManager({});
+        myMap.geoObjects.removeAll();
+        myMap.geoObjects.add(objectManager);
+
+        $.ajax({
+        url: "trains_circles.json"
+        }).done(function(data) {
+            objectManager.add(data);
+        });
+    };
+    document.getElementById('sea_circles').onclick = function () {
+        objectManager = new ymaps.ObjectManager({});
+        myMap.geoObjects.removeAll();
+        myMap.geoObjects.add(objectManager);
+
+        $.ajax({
+        url: "sea_circles.json"
+        }).done(function(data) {
+            objectManager.add(data);
+        });
+    };
+    document.getElementById('citytrains_circles').onclick = function () {
+        objectManager = new ymaps.ObjectManager({});
+        myMap.geoObjects.removeAll();
+        myMap.geoObjects.add(objectManager);
+
+        $.ajax({
+        url: "citytrains_circles.json"
+        }).done(function(data) {
+            objectManager.add(data);
+        });
+    };
+}
